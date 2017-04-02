d46ba27261c753b3305238de074f3bf4c0ccef16
Brian Hicks <brian@brianthicks.com>
2017-04-02 15:09:21 -0500

    first working version of the full header

--- elm/Styles.elm
+++ elm/Styles.elm
@@ -9,6 +9,11 @@ white =
     hex "FDFDFD"
 
 
+grey : Color
+grey =
+    hex "CCC"
+
+
 green : Color
 green =
     hex "7EBC42"
@@ -51,6 +56,9 @@ setup =
         , backgroundColor white
         , lineHeight <| squares 0.33
         ]
+    , everything
+        [ boxSizing borderBox
+        ]
     , each [ h1, h2, h3, h4, h5, h6 ]
         [ fontFamilies [ "Roboto Condensed", .value sansSerif ]
         , fontWeight bold
@@ -67,6 +75,8 @@ setup =
         , borderRadius zero
         , lineHeight zero
         ]
+    , selector "input[type=text], input[type=email]"
+        [ border3 (px 1) solid grey ]
     , selector "input[type=submit]"
         [ backgroundColor green
         , color white
@@ -81,6 +91,15 @@ setup =
 headerAndNav : List Snippet
 headerAndNav =
     let
+        logoWidth =
+            squares 4
+
+        margin =
+            squares 0.5
+
+        restWidth =
+            gridMaxWidth |-| logoWidth |-| margin
+
         masthead =
             class "logo"
                 [ backgroundColor green
@@ -88,19 +107,28 @@ headerAndNav =
                 , color white
                 , fontWeight bold
                 , padding2 (px 30) (px 60)
-                , marginRight (squares 0.5)
+                , marginRight margin
+                , width logoWidth
                 ]
 
         navLinks =
-            ul
-                [ listStyleType none
-                , padding zero
-                , children
-                    [ li
-                        [ children
-                            [ a
-                                [ color white
-                                , hover [ color green ]
+            nav
+                [ children
+                    [ ul
+                        [ listStyleType none
+                        , padding zero
+                        , displayFlex
+                        , flexDirection row
+                        , justifyContent flexEnd
+                        , children
+                            [ li
+                                [ children
+                                    [ a
+                                        [ color white
+                                        , hover [ color green ]
+                                        , lineHeight (squares 0.5)
+                                        ]
+                                    ]
                                 ]
                             ]
                         ]
@@ -112,6 +140,16 @@ headerAndNav =
                 [ flexDirection column
                 , property "text-align" "justify"
                 , alignSelf flexEnd
+                , children
+                    [ form
+                        [ displayFlex
+                        , marginBottom zero
+                        , children
+                            [ selector "input[type=email]"
+                                [ width (pct 100) ]
+                            ]
+                        ]
+                    ]
                 ]
     in
         [ header
@@ -123,13 +161,15 @@ headerAndNav =
             , backgroundColor white
             , backgroundRepeat noRepeat
             , children
-                [ nav
+                [ class "content"
                     [ gridContainer
                     , displayFlex
                     , flexDirection row
                     , justifyContent spaceBetween
+                    , height (squares 1.25)
                     , children
-                        [ masthead
+                        [ everything [ maxWidth restWidth ]
+                        , masthead
                         , navLinks
                         ]
                     ]
@@ -137,12 +177,21 @@ headerAndNav =
             , withClass "home"
                 [ backgroundImage (url "/images/header.png")
                 , backgroundPosition bottom
-                , paddingBottom <| squares 2
-                , flexDirection column
-                , descendants
-                    [ class "logo"
-                        [ paddingTop <| squares 2.5 ]
-                    , info
+                , paddingBottom (squares 1.5)
+                , children
+                    [ class "content"
+                        [ flexDirection column
+                        , flexWrap wrap
+                        , height (squares 5.5)
+                        , children
+                            [ info
+                            , class "logo"
+                                [ displayFlex
+                                , flexDirection columnReverse
+                                , flexBasis (pct 100)
+                                ]
+                            ]
+                        ]
                     ]
                 ]
             ]
